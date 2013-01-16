333e3d8a033ddb0bf56d0689894d3b1ba66f6d24
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-01-16 10:43:52 +0000

    Implemented get controller
    Implemented the get controller. Further exception handling pending

--- controllers/get.php
+++ controllers/get.php
@@ -11,4 +11,20 @@
  * @copyright Sunnefa Lind 2013
  * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
  */
+
+$page_slug = $_GET['page'];
+
+try {
+    $page = new Page($sql, $page_slug);
+    
+    foreach($page->page_modules as $dislay_order => $module) {
+        if($module->module_is_active == 1) {
+            include MODULES . $module->module_path;
+        }
+    }
+    
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
 ?>
