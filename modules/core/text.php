5f38d5e4f08a67a7472476e8e6adb4e5998110ca
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-01-16 19:30:30 +0000

    Created text module

---
+++ modules/core/text.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *  Text module
+ * 
+ *  Controller for the text module
+ * 
+ * @filename modules/core/text.php
+ * @package Core modules
+ * @version 0.1
+ * @author Sunnefa Lind <sunnefa_lind@hotmail.com>
+ * @copyright Sunnefa Lind 2013
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
+ */
+
+$page_data = array('page_id' => $page->page_id, 'display_order' => $display_order + 1);
+try {
+    $text_object = new Text($sql, $page_data);
+    echo $text_object->text;
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+?>
