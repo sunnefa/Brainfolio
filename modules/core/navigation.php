3ad25554e99495fe623c35d7727e721c33395353
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-01-16 17:53:48 +0000

    Created the navigation module
    Created the navigation module, which is kind of a submodule, at least
    for now, of header and footer...

---
+++ modules/core/navigation.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ *  Controller for the navigation
+ * 
+ *  Loads the navigation
+ * 
+ * @filename modules/core/navigation.php
+ * @package Core modules
+ * @version 0.1
+ * @author Sunnefa Lind <sunnefa_lind@hotmail.com>
+ * @copyright Sunnefa Lind 2013
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
+ */
+
+try {
+    
+    $navigation_html = '';
+    
+    foreach($pages_to_include as $num => $slug) {
+        $page_object = new Page($sql, $slug);
+        
+        if($page->page_slug == $slug) {
+            $class = 'current';
+        } elseif($num == count($pages_to_include) - 1) {
+            $class = 'last';
+        } else {
+            $class = '';
+        }
+        
+        $page_tokens = array(
+            'PAGE_SLUG' => $page_object->page_slug,
+            'PAGE_TITLE' => $page_object->page_title,
+            'BASE' => Functions::get_base_url(),
+            'CLASS' => $class
+        );
+        
+        $nav_item_template = new Template(TEMPLATES . 'core/nav_item.html', $page_tokens);
+        $navigation_html .= $nav_item_template->return_parsed_template();
+        
+    }
+    
+    /*$all_pages = $page->select_multiple_pages();
+    
+    $navigation_html = '';
+    
+    foreach($all_pages as $num => $single_page) {
+        
+        if($single_page['page_slug'] == $page->page_slug) {
+            $class = 'current';
+        } elseif($num == count($all_pages) - 1) {
+            $class = 'last';
+        } else {
+            $class = '';
+        }
+        
+        if($pages_to_include[$num] == $single_page['page_slug']) {
+            $single_tokens = array('PAGE_TITLE' => $single_page['page_title'], 'PAGE_SLUG' => $single_page['page_slug'], 'BASE' => Functions::get_base_url(), 'CLASS' => $class);
+            $template = new Template(TEMPLATES . 'core/nav_item.html', $single_tokens);
+            $navigation_html .= $template->return_parsed_template();
+        }
+    }*/
+    
+    $navigation_template = new Template(TEMPLATES . 'core/navigation.html', array('NAV_ITEMS' => $navigation_html));
+    
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+?>
