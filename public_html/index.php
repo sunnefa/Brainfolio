ffbe5a309e44c16f87ef65fbd402eb4d193ccc77
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-01-18 19:44:41 +0000

    Added throw/catch clause for mysqli_sql_exception in index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,9 +1,9 @@
 <?php
 /**
  *  Main index file
- * 
+ *
  *  This is the main index file, the sole access point to this application
- * 
+ *
  * @filename public_html/index.php
  * @package Controllers
  * @version 0.1
@@ -19,13 +19,18 @@ ob_start();
 if(!require_once('../config/init.php')) {
     die("The init file was not found!");
 }
+try {
+    $sql = MySQLWrapper::get_instance();
 
-$sql = MySQLWrapper::get_instance();
+    $settings = new Settings($sql);
 
-$settings = new Settings($sql);
-
-require_once CONTROLLERS . 'main.php';
-
+    require_once CONTROLLERS . 'main.php';
+} catch(mysqli_sql_exception $e) {
+    //todo: implement a prettier fallback for this such as include a file with an error message template
+    die('Unable to connect to database. The message returned was: ' . $e->getMessage());
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
 ob_end_flush();
 
 ?>
