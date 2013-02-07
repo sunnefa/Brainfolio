7b2721bed7f11f58e2ffb9a229c46baf3abc0fa8
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-02-07 22:22:31 +0000

    Changed get controller
    Started using the new get_query_strings function in the get controller
    and added support for maintenance mode. Also added support for page
    status and used center_die instead of just echo for the exception
    message

--- controllers/get.php
+++ controllers/get.php
@@ -12,18 +12,29 @@
  * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
  */
 
-$page_slug = $_GET['page'];
+$queries = Functions::get_query_strings();
+
+if($settings->maintenance_mode == 1) {
+    $page_slug = 'maintenance';
+} elseif(empty($queries)) {
+    $page_slug = 'home';
+} else {
+    $page_slug = $queries[0];
+}
 
 try {
     $page = new Page($sql, $page_slug);
-    
-    foreach($page->page_modules as $display_order => $module) {
-        if($module->module_is_active == 1) {
-            include MODULES . $module->module_path;
+    if($page->page_status != 0) {
+        foreach($page->page_modules as $display_order => $module) {
+            if($module->module_is_active == 1) {
+                include MODULES . $module->module_path;
+            }
         }
+    } else {
+        center_die('Page not found');
     }
     
 } catch(Exception $e) {
-    echo $e->getMessage();
+    center_die($e->getMessage());
 }
 ?>
