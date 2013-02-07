f8f6e12e30f983b1329ab9073b85b65a56000d09
Sunnefa Lind <sunnefa_lind@hotmail.com>
2013-02-07 22:24:08 +0000

    Created maintenance module

---
+++ modules/core/maintenance.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ *  Shows a message for maintenance mode
+ * 
+ *  When the site is in maintenance mode no parts of the site can be viewed (pending changes for a logged in user)
+ * 
+ * @filename modules/core/maintenance.php
+ * @package Core modules
+ * @version 0.1
+ * @author Sunnefa Lind <sunnefa_lind@hotmail.com>
+ * @copyright Sunnefa Lind 2013
+ * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3.0
+ */
+
+echo 'Maintenance mode is active';
+
+?>
